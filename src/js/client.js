a26953d2d99c96d5054254b47ab5eb2fe2bd95b1
Emil Pausz <emilpausz@gmail.com>
2016-04-30 20:12:59 +0200

    init

---
+++ src/js/client.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CSS from '../stylus/style.styl';
+
+import Layout from "./components/Layout";
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
